24532c630dbb699363dca8fed3b00c69fd13c05c
NVN404 <itsmenaveen404@gmail.com>
2025-10-31 10:16:24 +0530

    renamed folder

--- website/components/layout/footer.tsx
+++ website/components/layout/footer.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import Image from 'next/image';
 import { Logo } from '../icons/logo';
-import { placeHolderImages } from '@/lib/placeholder-images';
+import { placeHolderImages } from '../../lib/placeholder-images';
 import { Button } from '../ui/button';
 import Link from 'next/link';
 
